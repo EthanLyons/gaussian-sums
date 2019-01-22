0abafd94cd2172df25813281a51da1e1593ecdde
Ethan Lyons <ethan.lyons84@gmail.com>
2019-01-22 16:24:13 -0700

    Read upper limit from command line

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -1,16 +1,20 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class Gauss {
   public static final int DEFAULT_UPPER_LIMIT = 100;
 
   public static void main(String[] args) {
-
     int upperLimit = DEFAULT_UPPER_LIMIT;
-    if (args.length > 0)  {
-      upperLimit = Integer.parseInt(args[0]);
+    Scanner scanner = new Scanner(System.in); // Create Instance of Scanner Class, Using System.in as source.
+    System.out.println("Upper limit: ");
+    while (scanner.hasNextInt()) {
+      upperLimit = scanner.nextInt();
+      long sum = triangleSum(upperLimit);
+      System.out.printf("Sum from 1 to %,d = %,d%n", upperLimit, sum);
+      System.out.println("Upper limit: ");
     }
-    long sum = triangleSum(upperLimit);
-    System.out.printf("Sum from 1 to %,d = %,d%n", upperLimit, sum);
   }
 
   public static long triangleSum(int upperLimit)  {
